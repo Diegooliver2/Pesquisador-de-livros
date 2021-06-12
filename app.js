8ac582345ee402373f5a89100bf120da1ed86f94
DIEGO OLIVEIRA SILVA <diego.oliveiras@riachuelo.com.br>
2021-06-12 15:58:53 -0300

    Atualizando

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 async function getBooks(pesquisado) {
-    const request = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${pesquisado}&maxResults=10`)
+    const request = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${pesquisado}&maxResults=12`)
 
     return request.json()
 }
@@ -16,15 +16,12 @@ let prepBookForDOM = async (p) => {
     let cont = 0
 
     let databooks = data.items.map(x => {
+        console.log(x)
 
         let card = document.createElement('div')
         card.setAttribute('class', 'card')
         card.setAttribute('id', `card${++cont}`)
         card.setAttribute('onclick', `apresentacao('card${cont}')`)
-
-        let titulo = document.createElement('div')
-        titulo.setAttribute('class', 'titulo')
-        titulo.textContent = x.volumeInfo.title
 
         let descricao = document.createElement('div')
         descricao.setAttribute('class', 'descricao')
@@ -33,31 +30,45 @@ let prepBookForDOM = async (p) => {
         let capa = document.createElement('img')
         capa.setAttribute('class', 'capa')
         capa.src = x.volumeInfo.imageLinks.thumbnail
+        
+        let molde = document.createElement('div')
+        molde.setAttribute('class', 'molde')
+        molde.appendChild(capa)
+
+        let titulo = document.createElement('div')
+        titulo.setAttribute('class', 'titulo')
+        titulo.textContent = x.volumeInfo.title
 
         let dataPublicacao = document.createElement('div')
         dataPublicacao.setAttribute('class', 'dataPublicacao')
+        x.volumeInfo.publishedDate = x.volumeInfo.publishedDate !== undefined ? x.volumeInfo.publishedDate.split('-',1) : 'Sem data'
         dataPublicacao.textContent = x.volumeInfo.publishedDate
+
+        let dados = document.createElement('div')
+        dados.setAttribute('class', 'dados')
+        dados.appendChild(titulo)
+        dados.appendChild(dataPublicacao)
 
         let idBook = x.id
 
         return {
             card,
-            titulo,
             descricao,
-            capa, 
-            dataPublicacao, 
+            molde,
+            dados, 
             idBook
         }
-
     })
 
     return databooks
 }
 
 function addBooksInTheDOM(b){
+    document.querySelector('.areaDeResultados').innerHTML = ''
+
     for(let i = 0; i < b.length; i++){
-        b[i].card.appendChild(b[i].capa)
-        b[i].card.appendChild(b[i].titulo)
+        b[i].card.appendChild(b[i].molde)
+        b[i].card.appendChild(b[i].dados)
 
         document.querySelector('.areaDeResultados').appendChild(b[i].card)
     }
@@ -69,7 +80,4 @@ document.querySelector('.pesquisador').addEventListener('click', async () => {
     let a = await prepBookForDOM(tema.value)
 
     addBooksInTheDOM(a)
-    //
 })
-
-/**/
